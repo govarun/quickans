146455a37a7676bff8e87dd390ad661639d0de30
shradhasehgal <shradha.sgl@gmail.com>
2023-04-17 12:43:31 -0500

    Refactoring code changes

--- quickAns.py
+++ quickAns.py
@@ -13,19 +13,18 @@ import time
 openai.api_key = OPENAI_API_KEY
 MODEL = "gpt-3.5-turbo"
 PREFACE = f"I am a teaching assistant and a student in my course has posed the following question. What should the answer be? Question: "
-EMAIL_ID = "varun15@illinois.edu"
+EMAIL_ID = "ssehgal4@illinois.edu"
 QUESTION_ID_TO_EMAIL_COMPLETED_FILE = "question_id_to_emails_completed.txt"
 
-
 def read_previous_questions_answered():
-    Questions_to_emails_completed = set()
+    QuestionsToEmailsCompleted = set()
     if os.path.exists(QUESTION_ID_TO_EMAIL_COMPLETED_FILE):
         with open(QUESTION_ID_TO_EMAIL_COMPLETED_FILE, "r") as f:
             lines = f.readlines()
             for line in lines: 
                 question_id, email_id = line.split(",")
-                Questions_to_emails_completed.add((question_id, email_id))
-    return Questions_to_emails_completed
+                QuestionsToEmailsCompleted.add((question_id, email_id))
+    return QuestionsToEmailsCompleted
 
 async def greet_user(graph: Graph):
     user = await graph.get_user()
@@ -34,9 +33,9 @@ async def greet_user(graph: Graph):
         print('Email:', user.mail or user.user_principal_name, '\n')
 
 async def retrieve_emails(graph: Graph):
-    message_page = await graph.get_inbox()
-    if message_page is not None and message_page.value is not None:
-        return message_page.value
+    messagePage = await graph.get_inbox()
+    if messagePage is not None and messagePage.value is not None:
+        return messagePage.value
 
 async def send_mail(graph: Graph, subject, content, email=None):
 
@@ -46,7 +45,6 @@ async def send_mail(graph: Graph, subject, content, email=None):
             email = user.mail or user.user_principal_name
     print(subject, content, email)
     await graph.send_mail(subject, content, email)
-#         await graph.send_mail('Testing Microsoft Graph', 'Hello world!', "ssehgal4@illinois.edu" or '')
     print('Mail sent.\n')
 
 
@@ -63,17 +61,17 @@ def extract_relevant_and_uncompleted_questions(emailObjects, Questions_to_emails
     filteredQuestionIdToContent = {}
     for msg in emailObjects:
         bodyContent = msg._body._content
-        parsed_html = BeautifulSoup(bodyContent, "html.parser")
+        parsedHtml = BeautifulSoup(bodyContent, "html.parser")
         
-        heading = parsed_html.find("h1").text
+        heading = parsedHtml.find("h1").text
         if "asked a question in Advanced Information Retrieval" in heading:
             if (msg._id, EMAIL_ID) not in  Questions_to_emails_completed:
-                question = extract_question(parsed_html)
+                question = extract_question(parsedHtml)
                 filteredQuestionIdToContent[msg._id] = [heading, question]
     return filteredQuestionIdToContent
 
-def extract_question(parsed_html):
-    start = parsed_html.find("p", attrs={'class':'markdown_tester'})
+def extract_question(parsedHtml):
+    start = parsedHtml.find("p", attrs={'class':'markdown_tester'})
     question = f"{start.text}\n"
     for tr in start.find_next_siblings("p"):
         question += tr.text
@@ -86,7 +84,7 @@ def formulate_email_content(questionContent, answer):
     subject = f"QuickAns Assitant replies to '{questionContent[0]}'"
     emailContent = f"Hi there! I am QuickAns assistant and I am here to help you with questions on CampusWire.\n\n"
     emailContent += f"The following question was posed on CampusWire: \n\n``{questionContent[1]}``\n\n Here is a baseline response / some helpful tips to answer the question: \n\n ``{answer}`` \n\n" 
-    emailContent += "Thanks, Hope you have a great day on Campus (wire)!\n QuickAns Assistant"
+    emailContent += "Thanks, Hope you have a great day on Campus (Wire)!\n QuickAns Assistant"
     return subject, emailContent
 
 async def main():
@@ -108,7 +106,6 @@ async def main():
         await send_mail(graph, subject, emailContent, EMAIL_ID)
         with open(QUESTION_ID_TO_EMAIL_COMPLETED_FILE, "a") as f:
             f.write(f"{questionId},{EMAIL_ID}\n")
-        break
         time.sleep(20)
 
 # Run main
